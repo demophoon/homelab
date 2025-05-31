1b44ef069507d6993e67c99f2379ea4079e949bd
Britt Gresham <britt@brittg.com>
2025-05-31 11:50:35 -0700

    feat: Update approle restrictions

--- modules/cloudinit/approle/main.tf
+++ modules/cloudinit/approle/main.tf
@@ -6,6 +6,7 @@ data "vault_approle_auth_backend_role_id" "provision_role" {
 resource "vault_approle_auth_backend_role_secret_id" "provision_secret" {
   backend   = "approle"
   role_name = "vm_instance"
+  cidr_list = ["10.0.0.64/10"]
   metadata  = jsonencode({
     hostname = var.hostname
   })
