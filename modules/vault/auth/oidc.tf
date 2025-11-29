9cd8e782ead721c273bf20b7a01bdd1f5ebba797
Britt Gresham <britt@brittg.com>
2025-11-28 17:03:52 -0800

    Add oidc group bindings

--- modules/vault/auth/oidc.tf
+++ modules/vault/auth/oidc.tf
@@ -8,6 +8,30 @@ data "vault_auth_backend" "oidc" {
   path = "oidc"
 }
 
+resource "vault_identity_group" "ssh-user" {
+  name     = "SSH Users"
+  type     = "external"
+  policies = ["ssh-user"]
+}
+
+resource "vault_identity_group" "admin-user" {
+  name     = "Admin Users"
+  type     = "external"
+  policies = ["admin"]
+}
+
+resource "vault_identity_group_alias" "ssh-user-alias" {
+  name           = "Authentik SSH Users"
+  mount_accessor = data.vault_auth_backend.oidc.accessor
+  canonical_id   = vault_identity_group.ssh-user.id
+}
+
+resource "vault_identity_group_alias" "admin-user-alias" {
+  name           = "Authentik Admin Users"
+  mount_accessor = data.vault_auth_backend.oidc.accessor
+  canonical_id   = vault_identity_group.admin-user.id
+}
+
 resource "vault_jwt_auth_backend_role" "oidc-ssh-user" {
   backend         = data.vault_auth_backend.oidc.path
   role_name       = "ssh-user"
@@ -17,6 +41,7 @@ resource "vault_jwt_auth_backend_role" "oidc-ssh-user" {
 
   oidc_scopes           = ["openid", "profile", "email"]
   user_claim            = "email"
+  groups_claim          = "vault_groups"
   role_type             = "oidc"
   allowed_redirect_uris = [
     "https://vault-ui.internal.demophoon.com/ui/vault/auth/oidc/oidc/callback",
@@ -34,6 +59,7 @@ resource "vault_jwt_auth_backend_role" "oidc-admin" {
 
   oidc_scopes           = ["openid", "profile", "email"]
   user_claim            = "email"
+  groups_claim          = "vault_groups"
   role_type             = "oidc"
   allowed_redirect_uris = [
     "https://vault-ui.internal.demophoon.com/ui/vault/auth/oidc/oidc/callback",
