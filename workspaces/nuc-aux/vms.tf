a4d879e04b4e51216c2c78562fb41e940d4f3aef
Britt Gresham <britt@brittg.com>
2025-12-09 20:04:54 -0800

    Add miren.dev nodes!

--- workspaces/nuc-aux/vms.tf
+++ workspaces/nuc-aux/vms.tf
@@ -5,10 +5,30 @@ module "vm-nuc-2" {
   proxmox_node_prefix = "nuc-aux-1"
   proxmox_node_name = "nuc"
   proxmox_host = "192.168.1.35"
-  is_server = "true"
+  is_server = "false"
 
-  cpu = 4
-  memory = 14336
+  cpu = 2
+  memory = 4096
+  proxmox_ssh_user = var.proxmox_ssh_user
+  proxmox_ssh_password = var.proxmox_ssh_password
+  template_name = data.tfe_outputs.prod_home.values.nuc_template_id
+
+  tailscale_tailnet_name = var.tailscale_tailnet_name
+  workspace = "nuc-aux"
+  backplane_certificate = data.tfe_outputs.prod_home.values.backplane_certificate
+}
+
+module "vm-nuc-3" {
+  source = "../../modules/proxmox_vm"
+  resource = "module.vm-nuc"
+
+  proxmox_node_prefix = "nuc-aux-2"
+  proxmox_node_name = "nuc"
+  proxmox_host = "192.168.1.35"
+  is_server = "false"
+
+  cpu = 2
+  memory = 4096
   proxmox_ssh_user = var.proxmox_ssh_user
   proxmox_ssh_password = var.proxmox_ssh_password
   template_name = data.tfe_outputs.prod_home.values.nuc_template_id
