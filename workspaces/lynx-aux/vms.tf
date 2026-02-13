2c978bdf7a2a6c6b56e92f58171d318cbb5def29
Britt Gresham <britt@brittg.com>
2026-02-13 02:56:20 -0800

    Add lynx-aux and lynx-quo workspaces

---
+++ workspaces/lynx-aux/vms.tf
@@ -0,0 +1,29 @@
+module "vm-lynx-aux" {
+  source = "../../modules/proxmox_vm"
+  resource = "module.vm-lynx-aux"
+
+  proxmox_node_prefix = "lynx-aux-1"
+  proxmox_node_name = "lynx"
+  proxmox_host = "192.168.1.149"
+  is_server = "true"
+
+  cpu = 12
+  memory = 16384
+  persistant_disk = 64
+
+  proxmox_ssh_user = var.proxmox_ssh_user
+  proxmox_ssh_password = var.proxmox_ssh_password
+  template_name = data.tfe_outputs.prod_home.values.lynx_template_id
+
+  tailscale_tailnet_name = var.tailscale_tailnet_name
+  workspace = "lynx-aux"
+  backplane_certificate = data.tfe_outputs.prod_home.values.backplane_certificate
+
+  register_reprovision = true
+  reprovision_dow      = 3
+}
+
+module "lynx-reprovision" {
+  source = "../../modules/reprovisioner"
+  workspace = "lynx-aux"
+}
