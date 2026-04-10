84d1f2d710dd80f2d0b4a5ac2e64b512b794f145
Britt Gresham <britt@brittg.com>
2026-04-09 18:40:17 -0700

    Add renovate vault policy

---
+++ modules/vault/auth/renovate.tf
@@ -0,0 +1,29 @@
+resource "vault_jwt_auth_backend_role" "renovate" {
+  backend = vault_jwt_auth_backend.jwt-nomad.path
+  role_name = "renovate"
+  token_policies = ["renovate"]
+
+  bound_audiences = local.default_aud
+  user_claim = "/nomad_job_id"
+  role_type = "jwt"
+
+  claim_mappings = {
+    nomad_job_id    = "nomad_job_id"
+    nomad_namespace = "nomad_namespace"
+    nomad_task      = "nomad_task"
+  }
+
+  disable_bound_claims_parsing = false
+  token_period                 = 1800
+  token_type                   = "service"
+  user_claim_json_pointer      = true
+}
+
+resource "vault_policy" "renovate" {
+  name = "renovate"
+  policy = <<EOF
+path "kv/data/apps/renovate" {
+  capabilities = ["read"]
+}
+  EOF
+}
