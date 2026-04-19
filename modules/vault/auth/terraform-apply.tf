a25d9129270951da5813da19790ce00004025d9f
Britt Gresham <britt@brittg.com>
2026-04-18 22:52:44 -0700

    Allow terraform-apply to modify consul roles in vault

--- modules/vault/auth/terraform-apply.tf
+++ modules/vault/auth/terraform-apply.tf
@@ -163,6 +163,11 @@ resource "vault_policy" "terraform-apply" {
       capabilities = ["read", "create", "update", "delete", "sudo"]
     }
 
+    # Modify Consul roles
+    path "consul/roles/*" {
+      capabilities = ["read", "list", "create", "update", "delete"]
+    }
+
     # Vend Consul tokens
     path "consul/creds/*" {
       capabilities = ["read"]
