65a47e8986de3651ed40045127955789b9cc6152
Britt Gresham <britt@brittg.com>
2026-06-19 20:34:59 -0700

    Add anubis vault policy

---
+++ modules/vault/auth/anubis.tf
@@ -0,0 +1,29 @@
+resource "vault_jwt_auth_backend_role" "anubis" {
+  backend = vault_jwt_auth_backend.jwt-nomad.path
+  role_name = "anubis"
+  token_policies = ["anubis"]
+
+  bound_audiences = local.default_aud
+  user_claim = "/nomad_job_id"
+  role_type = "jwt"
+
+  claim_mappings = {
+    nomad_job_id    = "nomad_job_id"
+    nomad_namespace = "nomad_namespace"
+    nomad_task      = "nomad_task"
+  }
+
+  disable_bound_claims_parsing = false
+  token_period                 = 1800
+  token_type                   = "service"
+  user_claim_json_pointer      = true
+}
+
+resource "vault_policy" "anubis" {
+  name = "anubis"
+  policy = <<EOF
+path "kv/data/apps/anubis" {
+  capabilities = ["read"]
+}
+  EOF
+}
