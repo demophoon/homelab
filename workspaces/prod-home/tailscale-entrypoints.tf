b061c2b0956a418c9a3a4e3faf09775210f83b75
Britt Gresham <britt@brittg.com>
2026-08-03 09:27:17 -0700

    Dynamically pull vip from tailscale

--- workspaces/prod-home/tailscale-entrypoints.tf
+++ workspaces/prod-home/tailscale-entrypoints.tf
@@ -1,9 +1,13 @@
+data "tailscale_service" "internal" {
+  name = "svc:internal"
+}
+
 resource "google_dns_record_set" "demophoon-ts" {
   name         = "*.ts.demophoon.com."
   type         = "A"
   ttl          = 300
   managed_zone = google_dns_managed_zone.demophoon_com.name
 
-  rrdatas = ["100.113.204.81"]
+  rrdatas = tailscale_service.internal.addrs
 }
 
