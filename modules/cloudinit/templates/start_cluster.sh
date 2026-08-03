93a947163bc43b69e3093ccd7b6f5bdacada1769
Britt Gresham <britt@brittg.com>
2026-08-02 21:58:00 -0700

    Update ts. to point at internal load balancer

--- modules/cloudinit/templates/start_cluster.sh
+++ modules/cloudinit/templates/start_cluster.sh
@@ -52,8 +52,8 @@ start_tailscale_services() {
   tailscale serve --service=svc:consul --tcp=8302 tcp://${hostname}.blue-bowfin.ts.net:8302
   tailscale serve --service=svc:consul --tcp=8501 tcp://${hostname}.blue-bowfin.ts.net:8501
 
-  tailscale serve --service=svc:internal --tcp=80  tcp://${hostname}.blue-bowfin.ts.net:80
-  tailscale serve --service=svc:internal --tcp=443 tcp://${hostname}.blue-bowfin.ts.net:443
+  tailscale serve --service=svc:internal --tcp=80  localhost:80
+  tailscale serve --service=svc:internal --tcp=443 localhost:443
 }
 
 rm -f /etc/systemd/resolved.conf.d/DigitalOcean.conf
--- workspaces/prod-home/tailscale-entrypoints.tf
+++ workspaces/prod-home/tailscale-entrypoints.tf
@@ -1,13 +1,9 @@
-data "tailscale_devices" "proxmox" {
-  name_prefix = "lynx-"
-}
-
 resource "google_dns_record_set" "demophoon-ts" {
   name         = "*.ts.demophoon.com."
   type         = "A"
   ttl          = 300
   managed_zone = google_dns_managed_zone.demophoon_com.name
 
-  rrdatas = [for device in data.tailscale_devices.proxmox.devices: device.addresses[0]]
+  rrdatas = ["100.113.204.81"]
 }
 
