cbeefc7f1a2691a13d7d86064dfc6fe33d35c566
Britt Gresham <britt@brittg.com>
2026-09-24 20:31:31 -0700

    Keep approle token on hostname

--- modules/cloudinit/approle/main.tf
+++ modules/cloudinit/approle/main.tf
@@ -3,6 +3,12 @@ data "vault_approle_auth_backend_role_id" "provision_role" {
   role_name = "vm_instance"
 }
 
+resource "null_resource" "created_at" {
+  triggers = {
+    hostname = var.hostname
+  }
+}
+
 resource "vault_approle_auth_backend_role_secret_id" "provision_secret" {
   backend   = "approle"
   role_name = "vm_instance"
@@ -10,4 +16,15 @@ resource "vault_approle_auth_backend_role_secret_id" "provision_secret" {
   metadata  = jsonencode({
     hostname = var.hostname
   })
+
+  lifecycle {
+    ignore_changes = [
+      metadata,
+      ttl,
+      num_uses,
+    ]
+    replace_triggered_by = [
+      null_resource.created_at,
+    ]
+  }
 }
